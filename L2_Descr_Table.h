0e7bc0185800e347780fe7ffbe0de012a3be3137
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2023-05-25 14:52:36 +0200

    Added local copies

---
+++ L2_Descr_Table.h
@@ -0,0 +1,7 @@
+#ifndef _L2_DESCR_TABLE_H_
+#define _L2_DESCR_TABLE_H_
+#include "SFU_RT.h"
+extern SFU_RunTimeDescr_T * L2_RT_Descr_Table[1];
+#include "Graph_L2_Descr.h"
+#define SFU_Graph (0)
+#endif
