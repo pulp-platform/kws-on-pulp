6bb66029082ede9cbb46575ee16616940d342f0c
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2023-06-16 18:55:01 +0200

    Confirmed correct data acq with microphone

--- denoiser.c
+++ denoiser.c
@@ -442,51 +442,51 @@ int denoiser(void)
     }
     pi_cluster_task_stacks(task_mfcc, NULL, SLAVE_STACK_SIZE);
 
-    MfccInSig = (MFCC_IN_TYPE *) pi_l2_malloc(AUDIO_BUFFER_SIZE * sizeof(MFCC_IN_TYPE));
-    // MfccInSig = (MFCC_IN_TYPE *) pi_l2_malloc(BUFF_SIZE);
+    // MfccInSig = (MFCC_IN_TYPE *) pi_l2_malloc(AUDIO_BUFFER_SIZE * sizeof(MFCC_IN_TYPE));
+    MfccInSig = (MFCC_IN_TYPE *) pi_l2_malloc(BUFF_SIZE);
 
     
-    // /****
-    //     Setup the SFU for PDM in/out
-    // ****/
-    // struct pi_device i2s_sai1;
+    /****
+        Setup the SFU for PDM in/out
+    ****/
+    struct pi_device i2s_sai1;
 
-    // // Configure PDM
-    // if (open_i2s_PDM(&i2s_sai1, SAI1,   3072000, 3, 0)) return -1;
+    // Configure PDM
+    if (open_i2s_PDM(&i2s_sai1, SAI1,   3072000, 3, 0)) return -1;
 
-    // StartSFU(FREQ_SFU*1000*1000, 1);
-    // ChanOutCtxt_0  = (SFU_uDMA_Channel_T *) pi_l2_malloc(sizeof(SFU_uDMA_Channel_T));
+    StartSFU(FREQ_SFU*1000*1000, 1);
+    ChanOutCtxt_0  = (SFU_uDMA_Channel_T *) pi_l2_malloc(sizeof(SFU_uDMA_Channel_T));
 
 
-    // BufferInList = (void*) pi_l2_malloc(BUFF_SIZE);
+    BufferInList = (void*) pi_l2_malloc(BUFF_SIZE);
         
-    // // Get uDMA channels for Graph
-    // SFU_Allocate_uDMA_Channel(ChanOutCtxt_0, 0, &SFU_RTD(Graph));
+    // Get uDMA channels for Graph
+    SFU_Allocate_uDMA_Channel(ChanOutCtxt_0, 0, &SFU_RTD(Graph));
 
-    // //Next API will have a value to replace this high number with -1
-    // //To be able to 
-    // SFU_Enqueue_uDMA_Channel(ChanOutCtxt_0, BufferInList, BUFF_SIZE);
+    //Next API will have a value to replace this high number with -1
+    //To be able to 
+    SFU_Enqueue_uDMA_Channel(ChanOutCtxt_0, BufferInList, BUFF_SIZE);
     
-    // // Connect Channels to SFU for Mic IN (PDM IN)
-    // SFU_GraphConnectIO(SFU_Name(Graph, Out1), ChanOutCtxt_0->ChannelId, 0, &SFU_RTD(Graph));
-    // SFU_GraphConnectIO(SFU_Name(Graph, In1), SAI_ITF_IN, 2, &SFU_RTD(Graph));
+    // Connect Channels to SFU for Mic IN (PDM IN)
+    SFU_GraphConnectIO(SFU_Name(Graph, Out1), ChanOutCtxt_0->ChannelId, 0, &SFU_RTD(Graph));
+    SFU_GraphConnectIO(SFU_Name(Graph, In1), SAI_ITF_IN, 2, &SFU_RTD(Graph));
 
-    // pi_l2_free(ChanOutCtxt_0, sizeof(SFU_uDMA_Channel_T));
+    pi_l2_free(ChanOutCtxt_0, sizeof(SFU_uDMA_Channel_T));
 
-    // fxl6408_setup();
+    fxl6408_setup();
 
-    // printf("Recording!\n");
+    printf("Recording!\n");
 
-    // //Starting In and Out Graphs
-    // pi_i2s_ioctl(&i2s_sai1, PI_I2S_IOCTL_START, NULL);
-    // // Let the microphone start
-    // pi_time_wait_us(30000); 
+    //Starting In and Out Graphs
+    pi_i2s_ioctl(&i2s_sai1, PI_I2S_IOCTL_START, NULL);
+    // Let the microphone start
+    pi_time_wait_us(30000); 
 
-    // chunk_in_cnt=0;
-    // SFU_StartGraph(&SFU_RTD(Graph));
-    // pi_time_wait_us(2000000);
+    chunk_in_cnt=0;
+    SFU_StartGraph(&SFU_RTD(Graph));
+    pi_time_wait_us(2000000);
 
-    // pi_i2s_ioctl(&i2s_sai1, PI_I2S_IOCTL_STOP, NULL);
+    pi_i2s_ioctl(&i2s_sai1, PI_I2S_IOCTL_STOP, NULL);
 
 
 
@@ -507,37 +507,37 @@ int denoiser(void)
         // int round_out = (chunk_in_cnt>(STRUCT_DELAY-1))? ((chunk_in_cnt-(STRUCT_DELAY-1))%CHUNK_NUM):0;
 
 
-        // printf ("Scale data\n");
-        // int outidx = 0;
-        // for(int i=0;i<BUFF_SIZE;i+=3){
-        //     // We for now assume that no rescaling is needed
+        printf ("Scale data\n");
+        int outidx = 0;
+        for(int i=0;i<BUFF_SIZE;i+=3){
+            // We for now assume that no rescaling is needed
             
-        //     // MfccInSig[outidx] = ((MFCC_IN_TYPE *)BufferInList)[i];
-        //     // MfccInSig[outidx] = (MFCC_IN_TYPE) gap_clip( ( (int *) BufferInList)[i], 15);
-        //     // MfccInSig[outidx] = (MFCC_IN_TYPE)(((float)((int32_t*)BufferInList)[i]) /((int)(1<<Q_BIT_IN)));
+            // MfccInSig[outidx] = ((MFCC_IN_TYPE *)BufferInList)[i];
+            // MfccInSig[outidx] = (MFCC_IN_TYPE) gap_clip( ( (int *) BufferInList)[i], 15);
+            // MfccInSig[outidx] = (MFCC_IN_TYPE)(((float)((int32_t*)BufferInList)[i]) /((int)(1<<Q_BIT_IN)));
 
-        //     // WORKING
-        //     MfccInSig[outidx] = (MFCC_IN_TYPE)(((float)((int32_t*)BufferInList)[i]) /((int)(1<<16)));
-        //     outidx++;
-        //     if (outidx == AUDIO_BUFFER_SIZE){
-        //         break;
+            // WORKING
+            MfccInSig[outidx] = (MFCC_IN_TYPE)(((float)((int32_t*)BufferInList)[i]) /((int)(1<<16)));
+            outidx++;
+            if (outidx == AUDIO_BUFFER_SIZE){
+                break;
+            }
+            
+        }
+        pi_l2_free(BufferInList, BUFF_SIZE);
+
+        // // TODO: Measure error here versus only copying
+        // #if (DATA_TYPE==2) || (DATA_TYPE==3)
+        //     for (int i=0; i<AUDIO_BUFFER_SIZE; i++) { // BUFF_SIZE for MIC, AUDIO_BUFFER_SIZE for WAV
+        //         MfccInSig[i] = (MFCC_IN_TYPE) inWav[i] / (1<<15);
         //     }
-            
-        // }
-        // pi_l2_free(BufferInList, BUFF_SIZE);
-
-        // TODO: Measure error here versus only copying
-        #if (DATA_TYPE==2) || (DATA_TYPE==3)
-            for (int i=0; i<AUDIO_BUFFER_SIZE; i++) { // BUFF_SIZE for MIC, AUDIO_BUFFER_SIZE for WAV
-                MfccInSig[i] = (MFCC_IN_TYPE) inWav[i] / (1<<15);
-            }
-        #else
-            for (int i=0; i<AUDIO_BUFFER_SIZE; i++) { // BUFF_SIZE for MIC, AUDIO_BUFFER_SIZE for WAV
-                MfccInSig[i] = (MFCC_IN_TYPE) gap_clip(((int) inWav[i]), 15);
-                // MfccInSig[i] = (MFCC_IN_TYPE) gap_clip(((int) inWav[i]), 15); // TODO: 10 or 9 give absurdly better results
-            }
-        #endif
-        pi_l2_free(inWav, AUDIO_BUFFER_SIZE * sizeof(short));
+        // #else
+        //     for (int i=0; i<AUDIO_BUFFER_SIZE; i++) { // BUFF_SIZE for MIC, AUDIO_BUFFER_SIZE for WAV
+        //         MfccInSig[i] = (MFCC_IN_TYPE) gap_clip(((int) inWav[i]), 15);
+        //         // MfccInSig[i] = (MFCC_IN_TYPE) gap_clip(((int) inWav[i]), 15); // TODO: 10 or 9 give absurdly better results
+        //     }
+        // #endif
+        // pi_l2_free(inWav, AUDIO_BUFFER_SIZE * sizeof(short));
         
         out_feat = (OUT_TYPE *) pi_l2_malloc(49 * 10 * 4 * sizeof(OUT_TYPE));    
         feat_char = (char*) pi_l2_malloc(49 * 10 * sizeof(char));
