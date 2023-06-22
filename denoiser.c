63edc7a97a1ce9553f1d324d741bf8d94baf7ae4
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2023-06-22 13:49:48 +0200

    Naive addition of PULPTrainlib, tested on GAP9 GVSOC. TODO: Integrate.

--- denoiser.c
+++ denoiser.c
@@ -146,6 +146,12 @@ static PI_L2 uint8_t header_buffer[WAV_HEADER_SIZE];
 static struct pi_device fs_wav;
 static void *wavfile;
 
+
+// PULP TrainLib
+#include "net.h"
+
+
+
 void dump_wav_open(char *filename, int width, int sampling_rate, int nb_channels, int size)
 {
     unsigned int idx = 0;
@@ -737,7 +743,31 @@ int denoiser(void)
         // L2
         // network_run(L2_input, L2_MEMORY_SIZE, l2_buffer, 0, L2_input_h);
 
-        pi_l2_free(l2_buffer, L2_MEMORY_SIZE);
+
+        // clean buffer
+        // pi_l2_free(l2_buffer, L2_MEMORY_SIZE);
+
+
+        // run training (don't clean buffer yet)
+
+
+        struct pi_device cluster_dev;
+        struct pi_cluster_conf cl_conf;
+        struct pi_cluster_task cl_task;
+
+        pi_cluster_conf_init(&cl_conf);
+        pi_open_from_conf(&cluster_dev, &cl_conf);
+        if (pi_cluster_open(&cluster_dev))
+        {
+          return -1;
+        }
+
+        printf("\nLaunching training procedure...\n");
+        pi_cluster_send_task_to_cl(&cluster_dev, pi_cluster_task(&cl_task, net_step, NULL));
+
+        printf("Exiting DNN Training.\n");
+        pi_cluster_close(&cluster_dev);
+
 
         break;
 
