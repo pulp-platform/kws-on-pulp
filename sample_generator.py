7ad1f7606d909a60fabd5300a124ae120dfa6d87
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2023-12-09 21:46:00 +0100

    Added testing list

--- sample_generator.py
+++ sample_generator.py
@@ -28,14 +28,14 @@ def main():
     # Parse source dir
     filelist = [f for f in glob.glob("/usr/scratch/sassauna2/cioflanc/dolphinGSC/speech_commands_v0.02/*/*.wav")]
     
-    vallist_file = open("gsc_validation_list.txt", "r") 
+    vallist_file = open("testing_list.txt", "r") 
     vallist_txt = vallist_file.read() 
     vallist = vallist_txt.replace('\n', ' ') 
 
     random.shuffle(filelist)
 
-    shutil.rmtree('./validation')
-    os.mkdir('./validation')    
+    shutil.rmtree('./testing')
+    os.mkdir('./testing')    
 
     for file in filelist:
         file_name = file.split('/')[-1]
@@ -45,12 +45,12 @@ def main():
             file_name_path = word+'/'+file_name
             if word == file.split('/')[-2] and file_name_path in vallist:
            
-                shutil.copy(file, '/usr/scratch/wetterhorn/cioflanc/kws_on_gap9/tiny_denoiser_audiov2/tiny_denoiser/validation/'+word+'_'+ file.split('/')[-1])
-                file = '/usr/scratch/wetterhorn/cioflanc/kws_on_gap9/tiny_denoiser_audiov2/tiny_denoiser/validation/'+word+'_'+ file.split('/')[-1]
+                shutil.copy(file, '/usr/scratch/wetterhorn/cioflanc/kws_on_gap9/tiny_denoiser_audiov2/tiny_denoiser/testing/'+word+'_'+ file.split('/')[-1])
+                file = '/usr/scratch/wetterhorn/cioflanc/kws_on_gap9/tiny_denoiser_audiov2/tiny_denoiser/testing/'+word+'_'+ file.split('/')[-1]
                 sampledict[word]['samplelist'].append(file)
 
     # dump list
-    csv_file = 'validation.csv'
+    csv_file = 'testing.csv'
     with open(csv_file, 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
         writer.writeheader()
@@ -64,15 +64,15 @@ def main():
         for elem in sampledict[word]['samplelist']:
             dumplist.append(elem)
 
-    with open('validation.txt', 'w') as f:
+    with open('testing.txt', 'w') as f:
         for elem in dumplist:
             f.write(f"{elem}\n")
 
 
     # Arrange in .h to include it in app
-    with open('validation.h', 'w') as f:
-        f.write(f"#ifndef __VALIDATION_H__\n")
-        f.write(f"#define __VALIDATION_H__\n")
+    with open('testing.h', 'w') as f:
+        f.write(f"#ifndef __testing_H__\n")
+        f.write(f"#define __testing_H__\n")
 
         classidx = 0
         for word in sampledict:
