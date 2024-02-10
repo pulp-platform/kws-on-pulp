30957e7c900a846e35a329de1c8a1bb0f58e1f05
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-02-10 18:01:17 +0100

    Added MFCC QSNR computation and verified on 40 mels and 10 mels

--- verification/mfcc.py
+++ verification/mfcc.py
@@ -3,13 +3,14 @@ import argparse
 import torchaudio
 import torch
 import soundfile as sf
+import numpy as np
 
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--wavfile", type=str, default="../res/tinytest/backward_18f8afd5_nohash_1.wav", help=".wav file to read.")
-    parser.add_argument("--cmfcc", type=str, default="../build/mfccdump.data", help="MFCC generated in C.")
+    parser.add_argument("--wavfile", type=str, default="../testing/yes_563aa4e6_nohash_2.wav", help=".wav file to read.")
+    parser.add_argument("--cmfcc", type=str, default="../build/mfccdump.dat", help="MFCC generated in C.")
     args = parser.parse_args()
 
     # Load wav
@@ -18,20 +19,51 @@ def main():
 
 
     melkwargs={ 'n_fft':1024, 'win_length':640, 'hop_length':320,
-                             'f_min':20, 'f_max':4000, 'n_mels':10}
+                             'f_min':20, 'f_max':4000, 'n_mels':40}
 
     mfcc_transformation = torchaudio.transforms.MFCC(n_mfcc=10, sample_rate=16000, melkwargs=melkwargs, log_mels=True, norm='ortho')
     data = mfcc_transformation(wav_file)
+    data = data[:,:49]
 
     # Load C-generated MFCC
+    cdata = []
     with open(args.cmfcc, 'rb') as file:
         while 1:
             char = file.read(1)          
             if not char: 
                 break
-            print (ord(char))
+            cdata.append(ord(char))
 
-    # Compare
+
+    data = data.numpy().transpose().flatten()
+
+    # Rescale
+    for i in range(len(cdata)):
+        cdata[i] = (cdata[i] - 128)/0.1118  # 0.1118 = (pow(2, -1) * sqrt(0.05))
+
+    # Compare - QSNR
+    qsnr = 0   
+    mse = 0
+    gtsum = 0 
+    prsum = 0
+
+    for i in range(len(cdata)):
+        mse = mse + (data[i]-cdata[i])*(data[i]-cdata[i])
+        prsum = prsum + (cdata[i])*(cdata[i])
+        gtsum = gtsum + (data[i])*(data[i])
+
+    if (gtsum < mse):
+        qserrnr = -10*np.log10(mse/gtsum)
+        qquantsnr = -10*np.log10(prsum/gtsum)
+    else:
+        qserrnr =  10*np.log10(gtsum/mse)
+        qquantsnr = 10*np.log10(gtsum/prsum)
+    
+
+    print("qserrnr: ", qserrnr)
+    print("qquantsnr: ", qquantsnr)
+
+
 
 
 
