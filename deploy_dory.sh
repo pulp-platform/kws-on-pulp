b2675d3ce5f6b0d7f626424b2bf8a9b6dd89d644
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-03-18 18:57:06 +0100

    Cleaned codegen pipeline

--- deploy_dory.sh
+++ deploy_dory.sh
@@ -92,13 +92,13 @@ cp $CUR_DIR/config_DSCNN.json $NETWORK_DIR_SRC/ # TODO: .onnx path in config_DSC
 # Copy model and it's activations to Dory
 cd dory/
 mkdir -p $NETWORK_DIR_DEST_DORY
-rm $CUR_DIR/dory/$NETWORK_DIR_DEST_DOR/model.onnx
-rm $CUR_DIR/dory/$NETWORK_DIR_DEST_DOR/out_layer*.txt
-rm $CUR_DIR/dory/$NETWORK_DIR_DEST_DOR/input.txt
+rm $CUR_DIR/dory/$NETWORK_DIR_DEST_DORY/model.onnx
+rm $CUR_DIR/dory/$NETWORK_DIR_DEST_DORY/out_layer*.txt
+rm $CUR_DIR/dory/$NETWORK_DIR_DEST_DORY/input.txt
 
-cp $CUR_DIR/$NETWORK_DIR_SRC/input.txt $CUR_DIR/dory/$NETWORK_DIR_DEST_DOR/
-cp $CUR_DIR/$NETWORK_DIR_SRC/model.onnx  $CUR_DIR/dory/$NETWORK_DIR_DEST_DOR/
-cp $CUR_DIR/$NETWORK_DIR_SRC/out_layer*.txt $CUR_DIR/dory/$NETWORK_DIR_DEST_DOR/
+cp $CUR_DIR/$NETWORK_DIR_SRC/input.txt $CUR_DIR/dory/$NETWORK_DIR_DEST_DORY/
+cp $CUR_DIR/$NETWORK_DIR_SRC/model.onnx  $CUR_DIR/dory/$NETWORK_DIR_DEST_DORY/
+cp $CUR_DIR/$NETWORK_DIR_SRC/out_layer*.txt $CUR_DIR/dory/$NETWORK_DIR_DEST_DORY/
 
 # Generate source code and weights for model inference
 # We use 64 bits for the BatchNorm and ReLU
