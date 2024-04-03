f8e1810a0e053425fced8aa9c7bf0b6942f5e9a7
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-04-03 23:28:02 +0200

    Debugging

--- dscnn.py
+++ dscnn.py
@@ -36,14 +36,15 @@ class DSCNN(torch.nn.Module):
 
         self.stem = 'sym'
         self.padding =  'asym'
-        self.stem_block =  Conv_Stem_Sym(n_channels = self.n_channels, use_bias = self.use_bias, block_idx = 0).to(self.device)        
+        self.stem_block =  Conv_Stem_Sym(n_channels = self.n_channels, use_bias = self.use_bias).to(self.device)        
         self.pad_block = nn.ConstantPad2d((1, 1, 1, 0), value=0.)
         self.avg   = torch.nn.AvgPool2d(kernel_size=(20, 5), stride=1)
 
 
         self.conv_blocks_list = [] 
         for block_idx in range (0, self.n_blocks):
-            self.conv_blocks_list.append((DSCNN_block(n_channels = self.n_channels, use_bias = self.use_bias, block_idx = 2*n_blocks+1).to(self.device)))
+            self.conv_blocks_list.append((DSCNN_block(n_channels = self.n_channels, use_bias = self.use_bias).to(self.device)))
+            # self.conv_blocks_list.append((GenericConv2D(n_channels = 64, use_bias = self.use_bias).to(self.device)))
 
         self.conv_blocks = nn.ModuleList(self.conv_blocks_list)
 
@@ -89,11 +90,27 @@ class DSCNN(torch.nn.Module):
                 torch.nn.init.zeros_(m.bias)
 
 
-class Conv_Stem_Asym(torch.nn.Sequential):
-    def __init__(self, n_channels = 64, use_bias = True, block_idx = 0):
+class GenericConv2D(torch.nn.Sequential):
+    def __init__(self, n_channels = 64, use_bias = True):
 
         self.use_bias = use_bias
-        self.block_idx = block_idx
+
+        modules = []
+        modules += [torch.nn.Conv2d(in_channels = n_channels, out_channels = n_channels, kernel_size = (1, 1), stride = (1, 1), bias = self.use_bias)]
+        modules += [torch.nn.BatchNorm2d(n_channels)]
+        modules += [torch.nn.ReLU(inplace=True)]
+        modules += [torch.nn.Conv2d(in_channels = n_channels, out_channels = n_channels, kernel_size = (1, 1), stride = (1, 1), bias = self.use_bias)]
+        modules += [torch.nn.BatchNorm2d(n_channels)]
+        modules += [torch.nn.ReLU(inplace=True)]
+
+        super().__init__(*modules)
+
+
+
+class Conv_Stem_Asym(torch.nn.Sequential):
+    def __init__(self, n_channels = 64, use_bias = True):
+
+        self.use_bias = use_bias
 
         modules = []
         modules += [torch.nn.Conv2d(in_channels = 1, out_channels = n_channels, kernel_size = (10, 4), stride = (2, 2), bias = self.use_bias)]
@@ -104,10 +121,9 @@ class Conv_Stem_Asym(torch.nn.Sequential):
 
 
 class Conv_Stem_Sym(torch.nn.Sequential):
-    def __init__(self, n_channels = 64, use_bias = True, block_idx = 0):
+    def __init__(self, n_channels = 64, use_bias = True):
 
         self.use_bias = use_bias
-        self.block_idx = block_idx
 
         modules = []
         modules += [torch.nn.Conv2d(in_channels = 1, out_channels = n_channels, kernel_size = (3, 3), stride = (2, 2), bias = self.use_bias)]
@@ -118,13 +134,11 @@ class Conv_Stem_Sym(torch.nn.Sequential):
 
 
 class DSCNN_block(torch.nn.Sequential):
-    def __init__(self, n_channels = 64, use_bias = True, block_idx = 0):
+    def __init__(self, n_channels = 64, use_bias = True):
 
         self.use_bias = use_bias
-        self.block_idx = block_idx
 
         modules = []
-
         modules += [ torch.nn.Conv2d(in_channels = n_channels, out_channels = n_channels, kernel_size = (3, 3), stride = (1, 1), groups = n_channels, bias = self.use_bias) ]
         modules += [ torch.nn.BatchNorm2d(n_channels) ]
         modules += [ torch.nn.ReLU(inplace=True) ]
