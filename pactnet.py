2d35da49b999387325ff4eba57a72b13ec8b16fe
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-04-03 22:16:18 +0200

    Updated PACT rules following more complex MobileNetV2

--- pactnet.py
+++ pactnet.py
@@ -2,9 +2,9 @@
 # pact.py
 # 
 # Author(s):
-# Philip Wiese <wiesep@student.ethz.ch>
+# Georg Rutishauser <georgr@iis.ee.ethz.ch>
 # 
-# Copyright (c) 2023 ETH Zurich.
+# Copyright (c) 2020-2021 ETH Zurich.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,26 +19,41 @@
 # limitations under the License.
 # 
 
-from typing import List
+import json
+from typing import Optional
 
+import torch
 from torch import nn
 
 import quantlib.editing.lightweight as qlw
 from quantlib.editing.lightweight import LightweightGraph
 import quantlib.editing.lightweight.rules as qlr
-from quantlib.editing.lightweight.rules.filters import VariadicOrFilter, NameFilter, TypeFilter, SubTypeFilter
-from quantlib.editing.fx.passes.pact import HarmonizePACTNetPass, PACT_symbolic_trace, LeafTracer, PACT_OPS_INCLUSIVE
-from quantlib.editing.fx.passes import AnnotateEpsPass
+from quantlib.editing.lightweight.rules import LightweightRule
+from quantlib.editing.lightweight.rules.filters import VariadicOrFilter, NameFilter, TypeFilter
+from quantlib.editing.fx.passes.pact import HarmonizePACTNetPass, PACT_symbolic_trace
 
 from quantlib.algorithms.pact.pact_ops import *
 from quantlib.algorithms.pact.pact_controllers import *
-from quantlib.algorithms.pact.dynamic_precision import *
-
-
-
 
 def pact_recipe(net : nn.Module,
-                config : dict):
+                config : dict,
+                precision_spec_file : Optional[str] = None,
+                finetuning_ckpt : Optional[str] = None,
+                quantize_pool : bool = False):
+
+
+    if quantize_pool:
+        # in MNv1 we know the pooling node will be an adaptive avg pool
+        print("Quantizing Average Pooling layer...")
+        pool_filter = TypeFilter(nn.AdaptiveAvgPool2d)
+        pool_nodes = pool_filter(LightweightGraph.build_nodes_list(net))
+        act = nn.ReLU6
+        for n in pool_nodes:
+            print(f"Found pooling layer: {n.name}\nReplacing with Pool+ReLU6")
+            activated_pool = nn.Sequential(n.module, act(inplace=True))
+            LightweightRule.replace_module(net, n.name.split('.'), activated_pool)
+            if n.name+'.1' not in config["PACTUnsignedAct"].keys():
+                config["PACTUnsignedAct"][n.name+'.1'] = {}
 
     # config is expected to contain 3 keys for each layer type:
     # PACTConv2d, PACTLinear, PACTUnsignedAct
@@ -51,21 +66,39 @@ def pact_recipe(net : nn.Module,
 
     filter_conv2d = TypeFilter(nn.Conv2d)
     filter_linear = TypeFilter(nn.Linear)
-    filter_acts = TypeFilter(nn.ReLU)
-    # filter_softmax = TypeFilter(nn.LogSoftmax) #WIESEP: Not sure if this works
+    act_types = (nn.ReLU, nn.ReLU6)
+    filter_acts = VariadicOrFilter(*[TypeFilter(t) for t in act_types])
 
     rhos = []
     conv_cfg = config["PACTConv2d"]
     lin_cfg = config["PACTLinear"]
     act_cfg = config["PACTUnsignedAct"]
-    # softmax_cfg = config["PACTIntegerSoftmax"]
+
+    harmonize_cfg = config["harmonize"]
+
+
+    prec_override_spec = {}
+     # the precision_spec_file is (for example) dumped by a Bayesian Bits
+    # training run and overrides the 'n_levels' spec from config.json
+    if precision_spec_file is not None:
+        print(f"Overriding precision specification from config.json with spec from <{precision_spec_file}>...")
+        with open(precision_spec_file, 'r') as fh:
+            prec_override_spec = json.load(fh)['layer_levels']
+        # deal with nn.DataParallel wrapping
+
+        if all(k.startswith('module.') for k in prec_override_spec.keys()):
+            prec_override_spec = {k.lstrip('module.'):v for k,v in prec_override_spec.items()}
+        for cfg in (conv_cfg, lin_cfg, act_cfg):
+            appl_keys = [k for k in prec_override_spec.keys() if k in cfg.keys()]
+            for k in appl_keys:
+                cfg[k]['n_levels'] = prec_override_spec[k]
 
 
     def make_rules(cfg : dict,
                    rule : type):
         rules = []
         default_cfg = cfg["kwargs"] if "kwargs" in cfg.keys() else {}
-        layer_keys = [k for k in cfg.keys() if k != "kwargs" and k != "dynamic"]
+        layer_keys = [k for k in cfg.keys() if k != "kwargs"]
         for k in layer_keys:
             filt = NameFilter(k)
             kwargs = default_cfg.copy()
@@ -83,75 +116,48 @@ def pact_recipe(net : nn.Module,
 
     lwg = qlw.LightweightGraph(net)
     lwe = qlw.LightweightEditor(lwg)
-    
-    print("=== Original Network ===")
-    lwg.show_nodes_list()
-    
+
     lwe.startup()
     for rho in rhos:
         lwe.set_lwr(rho)
         lwe.apply()
     lwe.shutdown()
 
-    print("=== PACT Network ===")
-    lwg.show_nodes_list()
+    # now harmonize the graph according to the configuration
+    harmonize_pass = HarmonizePACTNetPass(**harmonize_cfg)
+    final_net = harmonize_pass(net)
 
-    return lwe._graph.net
+    # the prec. spec file might include layers that were added by the
+    # harmonization pass; those need to be treated separately
+    final_nodes = LightweightGraph.build_nodes_list(final_net)
+    for k, v in prec_override_spec.items():
+        if k.startswith("_QL"):
+            filt = NameFilter(k)
+            target_module = filt(final_nodes)[0].module
+            print(f"Setting module {k}'s 'n_levels' from {target_module.n_levels} to {v}...")
+            target_module.n_levels = v
 
-def build_module_spec(net : nn.Module, names : List[str], cfg : dict):
-    select_levels_dict = {"static": select_levels_static,
-                          "uniform": select_levels_uniform,
-                          "const": select_levels_const,
-                          "anneal": select_levels_anneal}
 
-    modules = [net.get_submodule(name) for name in names]
-    levels = cfg["levels"]
-    select_levels_val = select_levels_dict[cfg["select_levels_val"]](**cfg["val_kwargs"])
-    select_levels_trn = select_levels_dict[cfg["select_levels_trn"]](**cfg["trn_kwargs"])
-    return (modules, levels, select_levels_trn, select_levels_val)
+    if finetuning_ckpt is not None:
+        print(f"Loading finetuning ckpt from <{finetuning_ckpt}>...")
+        state_dict = torch.load(finetuning_ckpt)['net']
+        if all(k.startswith('module.') for k in state_dict.keys()):
+            state_dict = {k.lstrip('module.'):v for k,v in state_dict.items()}
+        final_net.load_state_dict(state_dict, strict=False)
 
-def get_pact_controllers(net : nn.Module, schedules : dict, kwargs_linear : dict = {}, kwargs_activation : dict = {}, dynamic : dict = {}):
-    net_nodes = LightweightGraph.build_nodes_list(net)
+
+    return final_net
+
+def get_pact_controllers(net : nn.Module, schedules : dict, kwargs_linear : dict = {}, kwargs_activation : dict = {}):
+    filter_intadd = TypeFilter(PACTIntegerAdd)
+    net_nodes_intadds_dissolved = LightweightGraph.build_nodes_list(net)
+    net_nodes_intadds_intact = LightweightGraph.build_nodes_list(net, leaf_types=(PACTIntegerAdd,))
     lin_modules = PACTLinearController.get_modules(net)
     act_modules = PACTActController.get_modules(net)
-
-    net_nodes = LightweightGraph.build_nodes_list(net)
-    filter_softmax = TypeFilter(PACTSoftmax, PACTITAMax, PACTITAPartialMax)
-    # softmax_modules =  [n.module for n in filter_softmax(net_nodes)]
+    intadd_modules = PACTIntegerModulesController.get_modules(net)
 
     lin_ctrl = PACTLinearController(lin_modules, schedules["linear"], **kwargs_linear)
     act_ctrl = PACTActController(act_modules, schedules["activation"], **kwargs_activation)
-    tracer = LeafTracer(PACT_OPS_INCLUSIVE)
-    # softmax_ctrl = PACTEpsController(net, softmax_modules, schedules["softmax"], tracer, AnnotateEpsPass(eps_in=1.0, n_levels_in=256))
-    try:
-        enable_dynamic = dynamic["cfg"]["enable"]
-    except KeyError:
-        enable_dynamic = False
+    intadd_ctrl = PACTIntegerModulesController(intadd_modules)
 
-    if enable_dynamic:
-        module_spec_list = []
-        module_cfgs = {k : v for k, v in dynamic.items() if k != "cfg"}
-        for mn, c in module_cfgs.items():
-            cfg = dynamic["cfg"].copy()
-            if "layers" in c.keys():
-                names = c["layers"]
-                try:
-                    config = c["cfg"]
-                except KeyError:
-                    config = {}
-            else:
-                names = [mn]
-                config = c
-            cfg.update(config)
-            module_spec_list.append(build_module_spec(net, names, cfg))
-
-        dyn_ctrl = PACTDynamicPrecController(module_spec_list)
-        #######################
-        # WATCH OUT!!!!!
-        # The order in which these controllers are returned matters!!!
-        # the dyn_ctrl must come first, because the step_xx functions are
-        # called in order on each controller!
-        ########################
-        return dyn_ctrl, lin_ctrl, act_ctrl
-
-    return lin_ctrl, act_ctrl
+    return lin_ctrl, act_ctrl, intadd_ctrl
