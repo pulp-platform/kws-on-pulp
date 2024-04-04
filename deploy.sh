e36c31164f9ee19e0a8f9b782fd715a74026aeec
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-04-04 16:42:34 +0200

    Building the application

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$1" == "-h" ] ; then
+    echo "PLATFORM: gvsoc, board"
+    echo "APPL: 0 (record)"
+    echo "NOISE EVAL: 0 (record)"
+    echo "UTTR EVAL: 0 (record)"
+    echo "MFCC computation: 0 (online)"
+    exit 0
+fi
+
+export PLATFORM=$1
+export APPL=$2
+export NOISE_EVAL=$3
+export UTTR_EVAL=$4
+export MFCC=$5
+
+export PATH=/usr/pack/gcc-9.2.0-af/linux-x64/bin:$PATH 
+export LD_LIBRARY_PATH=/usr/pack/gcc-9.2.0-af/linux-x64/lib64/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/scratch/wetterhorn/cioflanc/miniconda3/pkgs/mpfr-4.0.2-hb69a4c5_1/lib/
+export GAP_RISCV_GCC_TOOLCHAIN=/usr/scratch/wetterhorn/cioflanc/tools/gap_riscv_toolchain
+source /usr/scratch/wetterhorn/cioflanc/tools/gap_sdk_private/sourceme.sh # Choose your board/config
+source /usr/scratch/wetterhorn/cioflanc/tools/gap_sdk_private/configs/gap9_evk_audio.sh
+export WAV_FILE=/usr/scratch/wetterhorn/cioflanc/kws_on_gap9/tiny_denoiser/res/right_94de6a6a_nohash_4.wav # ORIGINAL
+
+cd application/
+
+/usr/scratch/wetterhorn/cioflanc/tools/cmake-3.19.5-Linux-x86_64/bin/cmake -B build
+/usr/scratch/wetterhorn/cioflanc/tools/cmake-3.19.5-Linux-x86_64/bin/cmake --build build --target menuconfig
+/usr/scratch/wetterhorn/cioflanc/tools/cmake-3.19.5-Linux-x86_64/bin/cmake --build build --target run
+
+
